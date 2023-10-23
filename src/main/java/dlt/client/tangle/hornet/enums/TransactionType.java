0c17bbabac421a2543e1a909203f81935f5e9ac2
AllanCapistrano <allan.capistrano3@gmail.com>
2023-10-23 09:25:04 -0300

    feat: add more reputation transactions type

--- src/main/java/dlt/client/tangle/hornet/enums/TransactionType.java
+++ src/main/java/dlt/client/tangle/hornet/enums/TransactionType.java
@@ -3,6 +3,7 @@ package dlt.client.tangle.hornet.enums;
 /**
  *
  * @author Uellington Damasceno, Allan Capistrano
+ * @version 1.1.0
  */
 public enum TransactionType {
   LB_ENTRY,
@@ -11,5 +12,9 @@ public enum TransactionType {
   LB_REQUEST,
   LB_REPLY,
   LB_DEVICE,
+  REP_SVC,
+  REP_SVC_REPLY,
+  REP_SVC_REQ,
+  REP_SVC_RES,
   REP_EVALUATION,
 }
