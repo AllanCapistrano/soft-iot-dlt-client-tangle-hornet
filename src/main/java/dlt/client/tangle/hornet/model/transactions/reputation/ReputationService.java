61f9024a6c9d94e904dd8f8aef454925fd06b594
AllanCapistrano <allan.capistrano3@gmail.com>
2023-10-27 10:16:51 -0300

    feat: add sourceIp

--- src/main/java/dlt/client/tangle/hornet/model/transactions/reputation/ReputationService.java
+++ src/main/java/dlt/client/tangle/hornet/model/transactions/reputation/ReputationService.java
@@ -6,23 +6,30 @@ import dlt.client.tangle.hornet.model.transactions.Transaction;
 import java.util.List;
 
 /**
- * Transaction to indicate that a service is needed.
+ * Transaction to indicate which services the node has.
  *
  * @author Allan Capistrano
  * @version 1.1.0
  */
 public class ReputationService extends Transaction {
 
+  private final String sourceIp;
   private final List<DeviceSensorId> services;
 
   public ReputationService(
-    String source,
+    String source,  
     String group,
+    String sourceIp,
     List<DeviceSensorId> services,
     TransactionType type
   ) {
     super(source, group, type);
+    this.sourceIp = sourceIp;
     this.services = services;
+  }
+
+  public String getSourceIp() {
+    return sourceIp;
   }
 
   public List<DeviceSensorId> getServices() {
