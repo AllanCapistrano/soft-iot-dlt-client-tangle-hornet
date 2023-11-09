87b58c5b358d839ed8e685f15aa5893c4ba58dcc
AllanCapistrano <allan.capistrano3@gmail.com>
2023-11-09 16:22:09 -0300

    chore: using TargetedTransaction instead of Transaction

--- src/main/java/dlt/client/tangle/hornet/model/transactions/reputation/ReputationService.java
+++ src/main/java/dlt/client/tangle/hornet/model/transactions/reputation/ReputationService.java
@@ -2,7 +2,7 @@ package dlt.client.tangle.hornet.model.transactions.reputation;
 
 import dlt.client.tangle.hornet.enums.TransactionType;
 import dlt.client.tangle.hornet.model.DeviceSensorId;
-import dlt.client.tangle.hornet.model.transactions.Transaction;
+import dlt.client.tangle.hornet.model.transactions.TargetedTransaction;
 import java.util.List;
 
 /**
@@ -11,19 +11,20 @@ import java.util.List;
  * @author Allan Capistrano
  * @version 1.1.0
  */
-public class ReputationService extends Transaction {
+public class ReputationService extends TargetedTransaction {
 
   private final String sourceIp;
   private final List<DeviceSensorId> services;
 
   public ReputationService(
-    String source,  
-    String group,
+    String source,
     String sourceIp,
+    String target,
     List<DeviceSensorId> services,
+    String group,
     TransactionType type
   ) {
-    super(source, group, type);
+    super(source, group, type, target);
     this.sourceIp = sourceIp;
     this.services = services;
   }
