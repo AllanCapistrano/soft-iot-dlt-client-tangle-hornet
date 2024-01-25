baa22477937be9adebe3f9b1c906e993214c91c2
AllanCapistrano <allan.capistrano3@gmail.com>
2024-01-25 09:36:28 -0300

    feat: add nodeCredibility and serviceEvaluation attributes

--- src/main/java/dlt/client/tangle/hornet/model/transactions/reputation/Evaluation.java
+++ src/main/java/dlt/client/tangle/hornet/model/transactions/reputation/Evaluation.java
@@ -7,10 +7,12 @@ import dlt.client.tangle.hornet.model.transactions.TargetedTransaction;
  * Transaction evaluating the service received.
  *
  * @author Allan Capistrano
- * @version 1.0.1
+ * @version 1.1.0
  */
 public class Evaluation extends TargetedTransaction {
 
+  private final float nodeCredibility;
+  private final int serviceEvaluation;
   private final float value;
 
   public Evaluation(
@@ -18,10 +20,22 @@ public class Evaluation extends TargetedTransaction {
     String target,
     String group,
     TransactionType type,
+    float nodeCredibility,
+    int serviceEvaluation,
     float value
   ) {
     super(source, group, type, target);
+    this.nodeCredibility = nodeCredibility;
+    this.serviceEvaluation = serviceEvaluation;
     this.value = value;
+  }
+
+  public int getServiceEvaluation() {
+    return serviceEvaluation;
+  }
+
+  public float getNodeCredibility() {
+    return nodeCredibility;
   }
 
   public float getValue() {
